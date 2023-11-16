9a5b970a77498029a2a5b8908e0a353669406187
FarzadVav <vav.progg.ir@gmail.com>
2023-11-16 02:49:18 -0800

    handle CustomNavLink component

---
+++ src/components/CustomNavLink.tsx
@@ -0,0 +1,53 @@
+import { ReactNode, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import LoadingContext from '../contexts/LoadingContext'
+
+type CustomNavLinkProps = {
+  className: string
+  activeClassName: string
+  path: string
+  conditionForActive: boolean
+  conditionForAlwaysActive: boolean
+  scrollToTop: boolean
+  clickHandler: Function
+  children: ReactNode
+}
+
+const CustomNavLink = ({
+  path,
+  activeClassName,
+  conditionForActive = true,
+  conditionForAlwaysActive = false,
+  className,
+  scrollToTop = true,
+  clickHandler,
+  children
+}: CustomNavLinkProps) => {
+  const loading = useContext(LoadingContext)
+  const navigate = useNavigate()
+
+  const _clickHandler = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault()
+    clickHandler()
+    scrollToTop && window.scrollTo(0, 0)
+    if (path !== location.pathname) {
+      loading.setPageLoadingHandler(true)
+      navigate(path)
+    }
+  }
+
+  return (
+    <a
+      className={(path === location.pathname && activeClassName && conditionForActive) || conditionForAlwaysActive
+        ? activeClassName
+        : className
+      }
+      onClick={_clickHandler}
+    >
+      {children}
+    </a>
+  )
+}
+
+export default CustomNavLink
