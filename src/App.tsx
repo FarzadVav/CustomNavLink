fb7e6eb8a3d92d390096689d31dd3e4f6ba4dcbc
FarzadVav <vav.progg.ir@gmail.com>
2023-11-16 03:00:13 -0800

    add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useRoutes } from "react-router-dom";
 
 import routes from "./routes";
 import { LoadingContextProvider } from "./contexts/LoadingContext";
+import Header from "./components/Header";
 
 const App = () => {
   const router = useRoutes(routes)
 
   return (
     <LoadingContextProvider>
+      <Header />
       {router}
     </LoadingContextProvider>
   )
